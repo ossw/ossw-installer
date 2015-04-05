d666e82bb8bbfa803d19e9f692357039c9d3dfe3
Krzysztof Szczesniak <k.szczesniak@althink.com>
2015-04-05 19:42:35 +0200

    initial commit based on sample app that writes to internal flash

---
+++ main_jump.c
@@ -0,0 +1,35 @@
+/* Copyright (c) 2009 Nordic Semiconductor. All Rights Reserved.
+ *
+ * The information contained herein is property of Nordic Semiconductor ASA.
+ * Terms and conditions of usage are described in detail in NORDIC
+ * SEMICONDUCTOR STANDARD SOFTWARE LICENSE AGREEMENT.
+ *
+ * Licensees are granted free, non-transferable use of the information. NO
+ * WARRANTY of ANY KIND is provided. This heading must NOT be removed from
+ * the file.
+ *
+ */
+
+/** @file
+ * @defgroup flashwrite_example_main main.c
+ * @{
+ * @ingroup flashwrite_example
+ *
+ * @brief This file contains the source code for a sample application using the Flash Write Application.
+ *
+ *
+ * @image html example_board_setup_a.jpg "Use board setup A for this example."
+ */
+
+#include "nrf.h"
+
+/**
+ * @brief Function for application main entry.
+ */
+int main(void)
+{
+    
+}
+
+
+/** @} */
