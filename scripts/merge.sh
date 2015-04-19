f8d45694ec5edf31299dbf3fc6550cb532826626
Krzysztof Szczesniak <k.szczesniak@althink.com>
2015-04-19 14:25:24 +0200

    merge script fix

--- scripts/merge.sh
+++ scripts/merge.sh
@@ -7,5 +7,5 @@ srec_cat s120_nrf51_2.0.0_softdevice.bin -binary -offset 0x19000 -o s120_at_0x19
 srec_cat ossw-bootloader-s120.hex -intel -exclude 0x10001000 0x100010fc -offset - -minimum-addr ossw-bootloader-s120.hex -intel -o ossw-bootloader-s120.bin -binary
 srec_cat ossw-bootloader-s120.bin -binary -offset 0x15000 -o ossw-bootloader-s120_at_0x15000.hex -intel
 
-srec_cat ossw-jump.hex -intel ossw-bootloader-s120_at_0x15000.hex -intel s120_at_0x19000.hex -intel ossw-installer_at_0x36000.hex -intel -o combined.hex -intel
-srec_cat combined.hex -intel -offset - -minimum-addr combined.hex -intel -o ossw-combined.bin -binary
+srec_cat ossw-jump.hex -intel ossw-bootloader-s120_at_0x15000.hex -intel s120_at_0x19000.hex -intel ossw-installer_at_0x36000.hex -intel -o ossw-combined.hex -intel
+srec_cat ossw-combined.hex -intel -offset - -minimum-addr ossw-combined.hex -intel -o ossw-combined.bin -binary
