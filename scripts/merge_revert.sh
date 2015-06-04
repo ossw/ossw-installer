05f263b505747d9c9d1d56aaaa0e22502f4f90da
Krzysztof Szczesniak <k.szczesniak@althink.com>
2015-06-04 22:22:20 +0200

    small fixes

--- scripts/merge_revert.sh
+++ scripts/merge_revert.sh
@@ -1,11 +1,11 @@
-srec_cat ossw-revert.hex -intel -offset - -minimum-addr ossw-installer.hex -intel -o ossw-installer.bin -binary
-srec_cat ossw-revert.bin -binary -offset 0x36000 -o ossw-installer_at_0x36000.hex -intel
+srec_cat ossw-revert.hex -intel -offset - -minimum-addr ossw-revert.hex -intel -o ossw-revert.bin -binary
+srec_cat ossw-revert.bin -binary -offset 0x3A000 -o ossw-revert_at_0x3A000.hex -intel
 
-srec_cat s120_nrf51_2.0.0_softdevice.hex -intel -offset - -minimum-addr s120_nrf51_2.0.0_softdevice.hex -intel -o s120_nrf51_2.0.0_softdevice.bin -binary
-srec_cat s120_nrf51_2.0.0_softdevice.bin -binary -offset 0x19000 -o s120_at_0x19000.hex -intel
+srec_cat chronos_fw.bin -binary -crop 0x0 0x14000 -o sd.bin -binary
+srec_cat sd.bin -binary -offset 0x26000 -o sd_at_0x26000.hex -intel
 
-srec_cat ossw-bootloader-s120.hex -intel -exclude 0x10001000 0x100010fc -offset - -minimum-addr ossw-bootloader-s120.hex -intel -o ossw-bootloader-s120.bin -binary
-srec_cat ossw-bootloader-s120.bin -binary -offset 0x15000 -o ossw-bootloader-s120_at_0x15000.hex -intel
+srec_cat chronos_fw.bin -binary -crop 0x38000 0x40000 -offset -0x38000 -o bl.bin -binary
+srec_cat bl.bin -binary -offset 0x1E000 -o bl_at_0x1E000.hex -intel
 
-srec_cat ossw-revert-jump.hex -intel ossw-bootloader-s120_at_0x15000.hex -intel s120_at_0x19000.hex -intel ossw-installer_at_0x36000.hex -intel -o ossw-revert-combined.hex -intel
+srec_cat ossw-revert-jump.hex -intel bl_at_0x1E000.hex -intel sd_at_0x26000.hex -intel ossw-revert_at_0x3A000.hex -intel -o ossw-revert-combined.hex -intel
 srec_cat ossw-revert-combined.hex -intel -offset - -minimum-addr ossw-revert-combined.hex -intel -o ossw-revert-combined.bin -binary
